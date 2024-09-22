e2590af22bcfcf0c0c8c1fbeb5b3f2d80501125d
danielrodrigues <drodrigues@isolutions.com.br>
2024-09-22 22:36:32 +0100

    First commit:
    - Project creation;
    - Dependencies configuration
    - Plugins configuration
    - Icons added;
    - Fonts added;

---
+++ androidApp/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.hilt.application)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.kotlinKapt)
+    alias(libs.plugins.kotlinSerialization)
+}
+
+android {
+    namespace = "br.com.codeforge.translator_kmm.android"
+    compileSdk = 34
+    defaultConfig {
+        applicationId = "br.com.codeforge.translator_kmm.android"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildFeatures {
+        compose = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(projects.shared)
+
+    // COMPOSE
+    implementation(libs.activity.compose)
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.icons.extended)
+    implementation(libs.compose.material3)
+    implementation(libs.compose.navigation)
+    implementation(libs.compose.ui)
+    debugImplementation(libs.compose.ui.tooling)
+    implementation(libs.compose.ui.tooling.preview)
+
+    // HILT
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    kapt(libs.hilt.compiler)
+    implementation(libs.hilt.navigation.compose)
+
+    implementation(libs.ktor.android)
+
+    androidTestImplementation(libs.testRunner)
+    androidTestImplementation(libs.jUnit)
+    androidTestImplementation(libs.composeTesting)
+    debugImplementation(libs.composeTestManifest)
+
+//    kaptAndroidTest(libs.hilt.android.compiler)
+    androidTestImplementation(libs.hiltTesting)
+}
---
+++ shared/build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.kotlinCocoapods)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.sqldelightPlugin)
+}
+
+kotlin {
+    android {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    cocoapods {
+        summary = "Some description for the Shared Module"
+        homepage = "Link to the Shared Module homepage"
+        setVersion("1.0")
+        ios.deploymentTarget = "16.0"
+        podfile = project.file("../iosApp/Podfile")
+        framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        sourceSets {
+            val commonMain by getting {
+                dependencies {
+                    implementation(libs.ktor.core)
+                    implementation(libs.ktor.serialization)
+                    implementation(libs.ktor.serialization.json)
+                    implementation(libs.sqldelight.runtime)
+                    implementation(libs.sqldelight.coroutines.extensions)
+                    implementation(libs.kotlin.date.time)
+                }
+            }
+            val commonTest by getting {
+                dependencies {
+                    implementation(libs.kotlin.test)
+                    implementation(libs.assertk)
+                    implementation(libs.turbine)
+                }
+            }
+            val androidMain by getting {
+                dependencies {
+                    implementation(libs.ktor.android)
+                    implementation(libs.sqldelight.android.driver)
+                }
+            }
+            val iosMain by creating {
+                dependencies {
+                    implementation(libs.ktor.ios)
+                    implementation(libs.sqldelight.native.driver)
+                }
+            }
+        }
+    }
+}
+
+android {
+    namespace = "br.com.codeforge.translator_kmm"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 28
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
