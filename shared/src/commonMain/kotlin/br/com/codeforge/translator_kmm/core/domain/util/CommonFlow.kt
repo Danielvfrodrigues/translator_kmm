7bc893dd563cb70061965385f7761223be78400b
danielrodrigues <drodrigues@isolutions.com.br>
2024-09-30 13:06:25 +0100

    Translate Feature - Shared: 2. Using Flows in KMM Projects

---
+++ shared/src/commonMain/kotlin/br/com/codeforge/translator_kmm/core/domain/util/CommonFlow.kt
@@ -0,0 +1,7 @@
+package br.com.codeforge.translator_kmm.core.domain.util
+
+import kotlinx.coroutines.flow.Flow
+
+expect open class CommonFlow<T>(flow: Flow<T>): Flow<T>
+
+fun <T> Flow<T>.toCommonFlow(): CommonFlow<T> = CommonFlow(this)
